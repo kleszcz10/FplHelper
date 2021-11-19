708c5596bcbd04f5f03bc394b9365e52d0807746
kleszcz10 <kleszczowiec@gmail.com>
2021-11-19 12:32:51 +0100

    Add MudBlazor

--- src/Fpl.Wasm/Program.cs
+++ src/Fpl.Wasm/Program.cs
@@ -1,12 +1,15 @@
+using Fpl.Core;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Fpl.Wasm
 {
@@ -19,6 +22,9 @@ namespace Fpl.Wasm
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddCore();
+            builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
         }
